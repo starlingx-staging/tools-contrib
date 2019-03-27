18079bac3e990292ac2ccb020474f6befc59deb8
VictorRodriguez <vm.rod25@gmail.com>
2019-03-27 16:26:49 -0600

    Add memory footprint
    Signed-off-by: VictorRodriguez <vm.rod25@gmail.com>

--- stx-metrics/metrics.py
+++ stx-metrics/metrics.py
@@ -30,13 +30,26 @@ def get_hd_footprint():
                     break
         else:
             break
-
     if sda:
         total_hd = (sda.split()[1])
         used_hd = (sda.split()[2])
         avail_hd = (sda.split()[3])
         per_hd = (sda.split()[4])
         return total_hd,used_hd,avail_hd,per_hd
+
+def memory_footprint(memory_kind):
+    proc = subprocess.Popen(['free','-h'],stdout=subprocess.PIPE)
+    while True:
+        line = proc.stdout.readline()
+        if line != '':
+            if memory_kind in line:
+              line_array = (line.rstrip().split())
+              mem_total = line_array[1]
+              mem_used = line_array[2]
+              break
+        else:
+            break
+    return mem_total,mem_used
 
 def main():
 
@@ -47,7 +60,9 @@ def main():
     parser.add_argument('--hd_footprint',\
         help='Print HD footprint',\
         action='store_true')
-
+    parser.add_argument('--memory_footprint',\
+        help='Print virtual memory footprint',\
+        action='store_true')
     args = parser.parse_args()
 
     if args.boottime:
@@ -60,6 +75,18 @@ def main():
         print("used_hd = " + used_hd)
         print("avail_hd = " + avail_hd)
         print("per_hd = " + per_hd)
+
+    if args.memory_footprint:
+        mem_total,mem_used = memory_footprint("Mem")
+        print ("\nMemory\n")
+        print ("    total = " + mem_total)
+        print ("    used = " + mem_used)
+
+        mem_total,mem_used = memory_footprint("Swap")
+        print ("\nSwap memory\n")
+        print ("    total = " + mem_total)
+        print ("    used = " + mem_used)
+
 
 if __name__ == "__main__":
     main()
