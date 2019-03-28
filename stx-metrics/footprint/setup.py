10a1f6d8b92ab2ef8dea26730ad229443e7363e1
VictorRodriguez <vm.rod25@gmail.com>
2019-03-28 16:46:50 -0600

    Enable setup scripts for build/install
    Signed-off-by: VictorRodriguez <vm.rod25@gmail.com>

---
+++ stx-metrics/footprint/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(name='metrics',
+      version='1.0',
+      description='Tool to help the track footprint',
+      author='Victor Rodriguez',
+      author_email='vm.rod25@gmail.com',
+      packages =['src'],
+      scripts=['src/metrics'],
+      )
+
