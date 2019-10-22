567fbeaebf4af113ddbdbff20138dca70721ec79
VictorRodriguez <vm.rod25@gmail.com>
2019-10-22 16:41:29 -0500

    clean up and fix
    Signed-off-by: VictorRodriguez <vm.rod25@gmail.com>

--- vuls-scan/resources/analyzer/main.py
+++ vuls-scan/resources/analyzer/main.py
@@ -1,47 +1,56 @@
+"""
+Implement policy based on
+https://wiki.openstack.org/wiki/StarlingX/Security/CVE_Support_Policy
+Create documentation as pydoc -w cve_policy_filter
+"""
 import json
 import sys
 import os
 
-cves = []
-cves_valid = []
-cves_to_fix = []
-cves_to_track = []
-cves_w_errors = []
-
-
 def chec_nvd_link(url):
+    """
+    If the NVD link exist it will fetch it for the report
+    return: True/False if the NVD link exist
+    """
     import requests
+    ret = False
     try:
         request = requests.get(url)
-    except:
-        print("No internet conection")
+    except ValueError as error:
+        print(error)
         sys.exit(-1)
     else:
         if request.status_code == 200:
-            return True
+            ret = True
         else:
-            return False
+            ret = False
+    return ret
 
-def print_html_report(title):
-
+def print_html_report(cves_to_fix, cves_to_track, cves_w_errors, title):
+    """
+    Print the html report
+    """
     import jinja2
 
-    templateLoader = jinja2.FileSystemLoader(searchpath="./")
-    templateEnv = jinja2.Environment(loader=templateLoader)
-    TEMPLATE_FILE = "template.txt"
-    template = templateEnv.get_template(TEMPLATE_FILE)
-    heads = ["cve_id","status","cvss2Score","av", "ac","au","ai"]
-    outputText = template.render(cves_to_fix = cves_to_fix,\
-        cves_to_track = cves_to_track,\
-        cves_w_errors = cves_w_errors,\
-        heads = heads,\
-        title = title)
-    html_file = open('report.html', 'w')
-    html_file.write(outputText)
+    template_loader = jinja2.FileSystemLoader(searchpath="./")
+    template_env = jinja2.Environment(loader=template_loader)
+    template_file = "template.txt"
+    template = template_env.get_template(template_file)
+    heads = ["cve_id", "status", "cvss2Score", "av", "ac", "au", "ai"]
+    output_text = template.render(cves_to_fix=cves_to_fix,\
+        cves_to_track=cves_to_track,\
+        cves_w_errors=cves_w_errors,\
+        heads=heads,\
+        title=title)
+    report_title = 'report_%s.html' % (title)
+    html_file = open(report_title, 'w')
+    html_file.write(output_text)
     html_file.close()
 
-def print_report():
-
+def print_report(cves_to_fix, cves_to_track, cves_w_errors):
+    """
+    Print the txt STDOUT report
+    """
     nvd_link = "https://nvd.nist.gov/vuln/detail/"
     print("\nValid CVEs to take action immediately: %d\n" % (len(cves_to_fix)))
     for cve in cves_to_fix:
@@ -56,8 +65,8 @@ def print_report():
         print("Affected packages:")
         print(cve["affectedpackages"])
         print(cve["summary"])
-        #if chec_nvd_link(nvd_link + cve["id"]):
-        #    print(nvd_link + cve["id"])
+        if chec_nvd_link(nvd_link + cve["id"]):
+            print(nvd_link + cve["id"])
 
     print("\nCVEs to track for incoming fix: %d \n" % (len(cves_to_track)))
     for cve in cves_to_track:
@@ -74,11 +83,19 @@ def print_report():
 
 
 def main():
+    """
+    main function
+    """
     data = {}
+    cves = []
+    cves_valid = []
+    cves_to_fix = []
+    cves_to_track = []
+    cves_w_errors = []
 
     if len(sys.argv) < 3:
         print("\nERROR : Missing arguments, the expected arguments are:")
-        print("\n   %s <result.json> <title>\n" % (sys.argv[0]) )
+        print("\n   %s <result.json> <title>\n" % (sys.argv[0]))
         print("\n result.json = json file generated from: vuls report -format-json")
         print("\n")
         sys.exit(0)
@@ -93,12 +110,12 @@ def main():
     try:
         with open(results_json) as json_file:
             data = json.load(json_file)
-    except ValueError as e:
-        print(e)
+    except ValueError as error:
+        print(error)
 
-    for p in data["scannedCves"]:
+    for element in data["scannedCves"]:
         cve = {}
-        cve["id"] = str(p.strip())
+        cve["id"] = str(element.strip())
         cves.append(cve)
 
     for cve in cves:
@@ -108,27 +125,27 @@ def main():
 
         try:
             nvd2_score = data["scannedCves"][cve_id]["cveContents"]["nvd"]["cvss2Score"]
-            cvss2vector  = data["scannedCves"][cve_id]["cveContents"]["nvd"]["cvss2Vector"]
+            cvss2vector = data["scannedCves"][cve_id]["cveContents"]["nvd"]["cvss2Vector"]
             summary = data["scannedCves"][cve_id]["cveContents"]["nvd"]["summary"]
             affectedpackages = data["scannedCves"][cve_id]["affectedPackages"]
 
-        except:
+        except KeyError:
             cves_w_errors.append(cve)
         else:
             cve["cvss2Score"] = nvd2_score
             for element in cvss2vector.split("/"):
                 if "AV:" in element:
-                    av = element.split(":")[1]
+                    _av = element.split(":")[1]
                 if "AC:" in element:
-                    ac = element.split(":")[1]
+                    _ac = element.split(":")[1]
                 if "Au:" in element:
-                    au = element.split(":")[1]
+                    _au = element.split(":")[1]
                 if "A:" in element:
-                    ai = element.split(":")[1]
-            cve["av"] = str(av)
-            cve["ac"] = str(ac)
-            cve["au"] = str(au)
-            cve["ai"] = str(ai)
+                    _ai = element.split(":")[1]
+            cve["av"] = str(_av)
+            cve["ac"] = str(_ac)
+            cve["au"] = str(_au)
+            cve["ai"] = str(_ai)
             cve["summary"] = str(summary)
             for pkg in affectedpackages:
                 affectedpackages_list.append(pkg["name"])
@@ -151,8 +168,8 @@ def main():
             else:
                 cves_to_track.append(cve)
 
-    print_report()
-    print_html_report(title)
+    print_report(cves_to_fix, cves_to_track, cves_w_errors)
+    print_html_report(cves_to_fix, cves_to_track, cves_w_errors, title)
 
 if __name__ == "__main__":
     main()
